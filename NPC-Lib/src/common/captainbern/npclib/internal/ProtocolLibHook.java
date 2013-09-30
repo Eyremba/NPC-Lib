47491dd31211733002aae68698be1b3d34a31030
CaptainBern <captainberneby@gmail.com>
2013-09-30 10:58:10 +0200

    Updated ProtocolLibHook

--- NPC-Lib/src/common/captainbern/npclib/internal/ProtocolLibHook.java
+++ NPC-Lib/src/common/captainbern/npclib/internal/ProtocolLibHook.java
@@ -1,5 +1,41 @@
 package common.captainbern.npclib.internal;
 
-public class ProtocolLibHook {
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ConnectionSide;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.reflect.FieldAccessException;
+
+import common.captainbern.npclib.NPCLib;
+
+public class ProtocolLibHook {
+	
+	public void setUpInteractListener(){
+		ProtocolManager pm = ProtocolLibrary.getProtocolManager();
+		
+		pm.addPacketListener(new PacketAdapter(NPCLib.instance, ConnectionSide.CLIENT_SIDE, 0x07){
+			 @Override
+             public void onPacketReceiving(PacketEvent event) {
+                 Player player = event.getPlayer();
+                 if (event.getPacketID() == 0x07) {
+                     try {
+                         PacketContainer packet = event.getPacket();
+                         int target = packet.getSpecificModifier(int.class).read(1);
+                         int action = packet.getSpecificModifier(int.class).read(2);
+                         
+                         if (packet.getEntityModifier(player.getWorld()).read(1) == null) {
+                        	 //
+                         }
+                     } catch (FieldAccessException e) {
+                         NPCLib.instance.log(ChatColor.RED + "Could not acces a field in packet 0x07! (used to listen for interaction with npc's!");
+                     }
+                 }
+             }
+		});
+	}
 }
