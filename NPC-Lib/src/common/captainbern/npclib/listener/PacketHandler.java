841cf2cd6608eadfac517bbc152cd5db81e2d37b
CaptainBern <captainberneby@gmail.com>
2013-10-03 15:44:55 +0200

    Created PacketHandler class; this will handle the incoming packets (eg: Packet7UseEntity, called when a player clicks on an entity)

---
+++ NPC-Lib/src/common/captainbern/npclib/listener/PacketHandler.java
@@ -0,0 +1,13 @@
+package common.captainbern.npclib.listener;
+
+import common.captainbern.reflection.ReflectionUtil;
+
+public class PacketHandler {
+
+    public void handlePacketAdd(Object packet){
+          if(packet.getClass().getName().equals("Packet7EntityUse")){
+
+          }
+    }
+
+}
