95d281637ee0a4ea50e91a0129bcdcab4be4cf3d
CaptainBern <captainberneby@gmail.com>
2013-10-03 15:46:47 +0200

    Don't really remember what I have changed so I would say... Bug fix!

--- NPC-Lib/src/common/captainbern/npclib/NPCLib.java
+++ NPC-Lib/src/common/captainbern/npclib/NPCLib.java
@@ -1,12 +1,18 @@
 package common.captainbern.npclib;
 
+import common.captainbern.npclib.entity.NPC;
+import common.captainbern.npclib.internal.PlayerHook;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import common.captainbern.npclib.internal.ProtocolLibHook;
 import common.captainbern.npclib.listener.PlayerListener;
+
+import java.util.LinkedList;
 
 public class NPCLib extends JavaPlugin{
 	
@@ -31,18 +37,49 @@ public class NPCLib extends JavaPlugin{
 		 * Check for ProtocolLib, if it is present then use that.
 		 */
 		if(pm.isPluginEnabled("ProtocolLib")){
-			log(ChatColor.GREEN + "Found ProtocolLib! Using that to listen for incomming packets...");
+			log(ChatColor.GREEN + "Found ProtocolLib! Using that to listen for incoming packets...");
 			usePL = true;
 			(new ProtocolLibHook()).setUpInteractListener();
 		}
+
+        if(!usePL){
+            PlayerHook hp = new PlayerHook();
+            for(Player player : Bukkit.getOnlinePlayers()){
+                hp.hookPlayer(player, true);
+            }
+        }
+
 	}
+
+    private class NPCUpdater implements  Runnable{
+
+        LinkedList<NPC> update_queue = new LinkedList<NPC>();
+
+        public void queuer(NPC npc){
+            if(!update_queue.contains(npc)){
+                update_queue.add(npc);
+            }else{
+                throw new RuntimeException("Cannot queue already queued NPC => {" + npc.getName() + "}");
+            }
+        }
+
+        @Override
+        public void run() {
+            for(NPC npc : update_queue){
+                //npc.update();
+                /*
+                update player connection
+                 */
+            }
+        }
+    }
 	
 	public NPCManager getNPCManager(){
 		return npcmanager;
 	}
 	
 	public void log(String message){
-		Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "[NPC-Lib] " + ChatColor.RESET + message);
+		Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "[NPC-Lib] " + ChatColor.RESET + message);
 	}
 
 }
