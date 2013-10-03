f015410bebf80305080efab2af00aa12204875fa
CaptainBern <captainberneby@gmail.com>
2013-10-03 15:45:25 +0200

    Updated listener class  a bit.

--- NPC-Lib/src/common/captainbern/npclib/listener/PlayerListener.java
+++ NPC-Lib/src/common/captainbern/npclib/listener/PlayerListener.java
@@ -3,6 +3,8 @@ package common.captainbern.npclib.listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class PlayerListener implements Listener {
 	
@@ -10,5 +12,15 @@ public class PlayerListener implements Listener {
 	public void onJoin(PlayerJoinEvent event){
 		 //hook player connection
 	}
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event){
+        //clear player connection stuff
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent event){
+        //check hook + send npc data.
+    }
 
 }
