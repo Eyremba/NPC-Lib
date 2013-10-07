a7db7d829d230b6f030a62ece2d93d933a6045f1
CaptainBern <captainberneby@gmail.com>
2013-10-07 09:30:22 +0200

    Space fixing.

--- NPC-Lib/src/common/captainbern/npclib/NPCManager.java
+++ NPC-Lib/src/common/captainbern/npclib/NPCManager.java
@@ -15,83 +15,83 @@ import org.bukkit.Location;
 import common.captainbern.npclib.entity.NPC;
 
 public class NPCManager {
-	
-	/**
-	 * We will use a LinkedList instead of an ArrayList since a LinkedList is faster and can't contain 2 times the
-	 * same value. (May change to arraylist in the future, who knows?)
-	 */
-	private LinkedList<NPC> npcs = new LinkedList<NPC>();
-	
-	private ConcurrentHashMap<Integer, NPC> npcIDS = new ConcurrentHashMap<Integer, NPC>();
-	private ConcurrentHashMap<String, NPC> npcNAMES = new ConcurrentHashMap<String, NPC>();
+
+    /**
+     * We will use a LinkedList instead of an ArrayList since a LinkedList is faster(- lol No) and can't contain 2 times the
+     * same value. (May change to arraylist in the future, who knows?)
+     */
+    private LinkedList<NPC> npcs = new LinkedList<NPC>();
+
+    private ConcurrentHashMap<Integer, NPC> npcIDS = new ConcurrentHashMap<Integer, NPC>();
+    private ConcurrentHashMap<String, NPC> npcNAMES = new ConcurrentHashMap<String, NPC>();
 
     private Object lock;
-	
-	public NPCManager(){
+
+    public NPCManager(){
         lock = new Object();
         synchronized (lock){
             // more code here
         }
-	}
+    }
 
-	public NPC createNpc(String name, Location location){
-		if(npcNAMES.containsKey(name)){
-			NPCLib.instance.log(ChatColor.RED + "There already exists an NPC with the name: " + name + "!");
-			return null;
-		}
-		
-		int id = getNextID();
-		
-		NPC npc = new NPC(name, location);
-		npc.setId(id);
-		npc.update();
-		
-		npcs.add(npc);
-		
-		npcNAMES.put(npc.getName(), npc);
-		npcIDS.put(id, npc);
-		
-		return npc;
-	}
-	
-	/**
-	 * Returns a fancy (unique) ID for the NPC.
-	 */
-	protected int nextID = Integer.MIN_VALUE;
-	private int getNextID(){
-		return nextID++;
-	}
-	
-	/**
-	 * Returns a npc by it's name.
-	 */
-	public NPC getNpcByName(String name){
-		if(npcNAMES.containsKey(name)){
-			return npcNAMES.get(name);
-		}else{
-			return null;
-		}
-	}
-	
-	/**
-	 * Returns a npc by it's id.
-	 */
-	public NPC getNpcById(int id){
-		if(npcIDS.containsKey(id)){
-			return npcIDS.get(id);
-		}else{
-			return null;
-		}
-	}
+    public NPC createNpc(String name, Location location){
+        if(npcNAMES.containsKey(name)){
+            NPCLib.instance.log(ChatColor.RED + "There already exists an NPC with the name: " + name + "!");
+            return null;
+        }
 
-	/**
-	 * Used to despawn all NPC's when the plugin get's disabled
-	 */
-	protected void despawnAll() {
-		for(NPC npc : npcs){
-			npcIDS.remove(npc.getId());
-			npcNAMES.remove(npc.getName());
-			npc.destroy();
-		}
-	}
+        int id = getNextID();
+
+        NPC npc = new NPC(name, location);
+        npc.setId(id);
+        npc.update();
+
+        npcs.add(npc);
+
+        npcNAMES.put(npc.getName(), npc);
+        npcIDS.put(id, npc);
+
+        return npc;
+    }
+
+    /**
+     * Returns a fancy (unique) ID for the NPC.
+     */
+    protected int nextID = Integer.MIN_VALUE;
+    private int getNextID(){
+        return nextID++;
+    }
+
+    /**
+     * Returns a npc by it's name.
+     */
+    public NPC getNpcByName(String name){
+        if(npcNAMES.containsKey(name)){
+            return npcNAMES.get(name);
+        }else{
+            return null;
+        }
+    }
+
+    /**
+     * Returns a npc by it's id.
+     */
+    public NPC getNpcById(int id){
+        if(npcIDS.containsKey(id)){
+            return npcIDS.get(id);
+        }else{
+            return null;
+        }
+    }
+
+    /**
+     * Used to despawn all NPC's when the plugin get's disabled
+     */
+    protected void despawnAll() {
+        for(NPC npc : npcs){
+            npcIDS.remove(npc.getId());
+            npcNAMES.remove(npc.getName());
+            npc.destroy();
+        }
+    }
 }
