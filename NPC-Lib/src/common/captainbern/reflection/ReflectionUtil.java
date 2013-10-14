525cdc6f031c562a7354ed1f68129545199b4d47
CaptainBern <captainberneby@gmail.com>
2013-10-14 14:44:23 +0200

    Added some methods to get obc classes.

--- NPC-Lib/src/common/captainbern/reflection/ReflectionUtil.java
+++ NPC-Lib/src/common/captainbern/reflection/ReflectionUtil.java
@@ -10,7 +10,7 @@ import org.bukkit.entity.Player;
 public class ReflectionUtil {
 	
 	public static Object getNMSClass(String name, Object... args) throws Exception {
-		Class<?> c = Class.forName(ReflectionUtil.getPackageName() + "." + name);
+		Class<?> c = Class.forName(ReflectionUtil.getNMSPackageName() + "." + name);
 		int params = 0;
 		if (args != null) {
 			params = args.length;
@@ -26,12 +26,36 @@ public class ReflectionUtil {
 	public static Class<?> getNMSClassExact(String name, Object... args){
 		Class<?> c;
 		try{
-		c= Class.forName(ReflectionUtil.getPackageName() + "." + name);
+		c= Class.forName(ReflectionUtil.getNMSPackageName() + "." + name);
 		}catch(Exception e){
 			return null;
 		}
 		return c;
 	}
+
+    public static Object getOBCClass(String name, Object... args) throws Exception {
+        Class<?> c = Class.forName(ReflectionUtil.getOBCPackageName() + "." + name);
+        int params = 0;
+        if (args != null) {
+            params = args.length;
+        }
+        for (Constructor<?> co : c.getConstructors()) {
+            if (co.getParameterTypes().length == params) {
+                return co.newInstance(args);
+            }
+        }
+        return null;
+    }
+
+    public static Class<?> getOBCClassExact(String name, Object... args){
+        Class<?> c;
+        try{
+            c= Class.forName(ReflectionUtil.getOBCPackageName() + "." + name);
+        }catch(Exception e){
+            return null;
+        }
+        return c;
+    }
 
 	public static Method getMethod(String name, Class<?> c, int params) {
 		for (Method m : c.getMethods()) {
@@ -66,9 +90,13 @@ public class ReflectionUtil {
 		field.set(instance, value);
 	}
 
-	public static String getPackageName() {
+	public static String getNMSPackageName() {
 		return "net.minecraft.server." + Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
 	}
+
+    public static String getOBCPackageName(){
+        return "org.bukkit.craftbukkit" + Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+    }
 	
 	public static Object BukkitPlayerToEntityPlayer(Player player){
 		Object entityPlayer = null;
