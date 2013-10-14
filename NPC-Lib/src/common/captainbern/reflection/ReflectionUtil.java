a81a5f3ad1f95f926e244cb99aec9fabdd48a9ee
CaptainBern <captainberneby@gmail.com>
2013-10-14 14:46:21 +0200

    Fixed spacing.

--- NPC-Lib/src/common/captainbern/reflection/ReflectionUtil.java
+++ NPC-Lib/src/common/captainbern/reflection/ReflectionUtil.java
@@ -8,30 +8,30 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class ReflectionUtil {
-	
-	public static Object getNMSClass(String name, Object... args) throws Exception {
-		Class<?> c = Class.forName(ReflectionUtil.getNMSPackageName() + "." + name);
-		int params = 0;
-		if (args != null) {
-			params = args.length;
-		}
-		for (Constructor<?> co : c.getConstructors()) {
-			if (co.getParameterTypes().length == params) {
-				return co.newInstance(args);
-			}
-		}
-		return null;
-	}
-	
-	public static Class<?> getNMSClassExact(String name, Object... args){
-		Class<?> c;
-		try{
-		c= Class.forName(ReflectionUtil.getNMSPackageName() + "." + name);
-		}catch(Exception e){
-			return null;
-		}
-		return c;
-	}
+
+    public static Object getNMSClass(String name, Object... args) throws Exception {
+        Class<?> c = Class.forName(ReflectionUtil.getNMSPackageName() + "." + name);
+        int params = 0;
+        if (args != null) {
+            params = args.length;
+        }
+        for (Constructor<?> co : c.getConstructors()) {
+            if (co.getParameterTypes().length == params) {
+                return co.newInstance(args);
+            }
+        }
+        return null;
+    }
+
+    public static Class<?> getNMSClassExact(String name, Object... args){
+        Class<?> c;
+        try{
+            c= Class.forName(ReflectionUtil.getNMSPackageName() + "." + name);
+        }catch(Exception e){
+            return null;
+        }
+        return c;
+    }
 
     public static Object getOBCClass(String name, Object... args) throws Exception {
         Class<?> c = Class.forName(ReflectionUtil.getOBCPackageName() + "." + name);
@@ -57,55 +57,55 @@ public class ReflectionUtil {
         return c;
     }
 
-	public static Method getMethod(String name, Class<?> c, int params) {
-		for (Method m : c.getMethods()) {
-			if (m.getName().equals(name) && m.getParameterTypes().length == params) {
-				return m;
-			}
-		}
-		return null;
-	}
-	
-	public static Method getMethod(String name, Class<?> c) {
-		for (Method m : c.getMethods()) {
-			if (m.getName().equals(name)) {
-				return m;
-			}
-		}
-		return null;
-	}
+    public static Method getMethod(String name, Class<?> c, int params) {
+        for (Method m : c.getMethods()) {
+            if (m.getName().equals(name) && m.getParameterTypes().length == params) {
+                return m;
+            }
+        }
+        return null;
+    }
 
-	public static Field getField(String name, Class<?> c) {
-		for (Field f : c.getFields()) {
-			if (f.getName().equals(name)) {
-				return f;
-			}
-		}
-		return null;
-	}
+    public static Method getMethod(String name, Class<?> c) {
+        for (Method m : c.getMethods()) {
+            if (m.getName().equals(name)) {
+                return m;
+            }
+        }
+        return null;
+    }
 
-	public static void setValue(Object instance, String fieldName, Object value) throws Exception {
-		Field field = instance.getClass().getDeclaredField(fieldName);
-		field.setAccessible(true);
-		field.set(instance, value);
-	}
+    public static Field getField(String name, Class<?> c) {
+        for (Field f : c.getFields()) {
+            if (f.getName().equals(name)) {
+                return f;
+            }
+        }
+        return null;
+    }
 
-	public static String getNMSPackageName() {
-		return "net.minecraft.server." + Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
-	}
+    public static void setValue(Object instance, String fieldName, Object value) throws Exception {
+        Field field = instance.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(instance, value);
+    }
+
+    public static String getNMSPackageName() {
+        return "net.minecraft.server." + Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+    }
 
     public static String getOBCPackageName(){
         return "org.bukkit.craftbukkit" + Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
     }
-	
-	public static Object BukkitPlayerToEntityPlayer(Player player){
-		Object entityPlayer = null;
-		try{
-		entityPlayer = ReflectionUtil.getMethod("getHandle", player.getClass(), 0).invoke(player);
-		}catch(Exception e){
-			return null;
-		}
-		return entityPlayer;
-	}
+
+    public static Object BukkitPlayerToEntityPlayer(Player player){
+        Object entityPlayer = null;
+        try{
+            entityPlayer = ReflectionUtil.getMethod("getHandle", player.getClass(), 0).invoke(player);
+        }catch(Exception e){
+            return null;
+        }
+        return entityPlayer;
+    }
 
 }
